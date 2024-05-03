1b79f023f8c8a13edc54290af09651c20d9399a7
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-05-03 15:03:25 -0400

    Initial Commit

---
+++ MenuAR/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MenuAR
+//
+//  Created by Enrique Sarmiento on 3/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ MenuAR/MenuARApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuARApp.swift
+//  MenuAR
+//
+//  Created by Enrique Sarmiento on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
